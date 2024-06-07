4f83f0c75be23ad1e3921b587ed422a0e52c5587
Rohan <mrohan9701@gmail.com>
2024-06-07 15:24:07 +0530

    final test

--- src/test/java/com/magicalarena/GameTest.java
+++ src/test/java/com/magicalarena/GameTest.java
@@ -1,17 +1,20 @@
 package com.magicalarena;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class GameTest {
     @Test
     public void testGameEnds() {
         Player playerA = new Player(10, 5, 10);
         Player playerB = new Player(10, 10, 5);
-
         Game game = new Game(playerA, playerB);
-        game.start();
 
-        assertFalse(playerA.isAlive() || playerB.isAlive());
+        game.start();
+        
+        // One of the players should have health greater than 0, and the other should be defeated.
+        assertTrue("One player should have health > 0 and the other should be defeated", 
+                   (playerA.getHealth() > 0 && playerB.getHealth() <= 0) || 
+                   (playerA.getHealth() <= 0 && playerB.getHealth() > 0));
     }
 }
